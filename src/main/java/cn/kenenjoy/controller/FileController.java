91f31b0d24de857b0d7172f7634c76f04cb7e9df
何发 <207324509@qq.com>
2017-12-13 17:56:36 +0800

    add File Upload

--- src/main/java/cn/kenenjoy/controller/FileController.java
+++ src/main/java/cn/kenenjoy/controller/FileController.java
@@ -1,7 +1,61 @@
 package cn.kenenjoy.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+
 /**
  * Created by hefa on 2017/12/13.
  */
+@Controller
+@RequestMapping(value = "/file")
 public class FileController {
+    private static final Logger log = LoggerFactory.getLogger(FileController.class);
+
+    /**
+     * 处理文件上传
+     *
+     * @param request
+     * @return
+     * @throws IOException
+     */
+    @RequestMapping(value = "/uploadFiles", method = RequestMethod.POST)
+    @ResponseBody
+    public static Map<String, String> uploads(HttpServletRequest request) throws IOException {
+        Map<String, String> result = new HashMap<String, String>();
+        //文件上传的请求
+        MultipartHttpServletRequest multipartHttpServletRequest = (MultipartHttpServletRequest) request;
+        //获取请求的参数
+        Map<String, MultipartFile> fileMap = multipartHttpServletRequest.getFileMap();
+        Iterator<Map.Entry<String, MultipartFile>> it = fileMap.entrySet().iterator();
+
+        while (it.hasNext()) {
+            Map.Entry<String, MultipartFile> entry = it.next();
+            MultipartFile multipartFile = entry.getValue();
+            log.info("文件名称：" + multipartFile.getName() + "\t 文件大小：" + multipartFile.getSize());
+            if (multipartFile.getSize() != 0 && !"".equals(multipartFile.getName())) {
+                // 文件保存路径和文件名称，默认为参数文件名
+                File file = new File(multipartFile.getName());
+                FileOutputStream outputStream = new FileOutputStream(file);
+                outputStream.write(multipartFile.getBytes());
+                outputStream.close();
+                result.put(multipartFile.getName(), multipartFile.getOriginalFilename());
+            }
+        }
+        return result;
+    }
 }
--- src/test/java/cn/kenenjoy/FileUploadTest.java
+++ src/test/java/cn/kenenjoy/FileUploadTest.java
@@ -1,7 +1,39 @@
 package cn.kenenjoy;
 
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by hefa on 2017/12/13.
  */
 public class FileUploadTest {
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpPost httppost = new HttpPost("http://127.0.0.1:8080/file/uploadFiles");
+        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+        File file = new File("/Users/hefa/Downloads/abc.txt");
+        builder.addBinaryBody("abc.txt", file);
+        builder.addBinaryBody("abc2.txt", file);
+        httppost.setEntity(builder.build());
+        CloseableHttpResponse response = httpclient.execute(httppost);
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            System.out.println("成功！");
+            // 响应结果
+            HttpEntity entity = response.getEntity();
+            System.out.println(EntityUtils.toString(entity));
+        } else {
+            System.out.println("失败！");
+        }
+        response.close();
+    }
 }
