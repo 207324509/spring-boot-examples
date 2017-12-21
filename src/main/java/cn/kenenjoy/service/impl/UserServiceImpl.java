0c78c473dc7a8d068892a8657b7cd19119bac52e
何发 <207324509@qq.com>
2017-12-21 23:20:09 +0800

    命名空间优化

---
+++ src/main/java/cn/kenenjoy/service/impl/UserServiceImpl.java
@@ -0,0 +1,66 @@
+package cn.kenenjoy.service.impl;
+
+import cn.kenenjoy.domain.User;
+import cn.kenenjoy.mapper.UserMapper;
+import cn.kenenjoy.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+/**
+ * Created by hefa on 2017/7/28.
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    private static final Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Cacheable(value = "users")
+    @Override
+    public User findUserById(String id) {
+        return userMapper.findUserById(id);
+    }
+
+    @Cacheable(value = "users")
+    @Override
+    public List<User> getUsers(String sort, String order, int page, int rows) {
+        log.debug("getUsers查询数据库");
+        return userMapper.getUsers(sort, order, (page-1)*rows, rows);
+    }
+
+    @Cacheable(value = "users")
+    @Override
+    public Integer countUsers() {
+        return userMapper.countUsers();
+    }
+
+
+    @CacheEvict(value = {"users"}, allEntries = true)
+    @Override
+    public void saveUser(User user) {
+        userMapper.saveUser(user);
+    }
+
+    @CacheEvict(value = {"users"}, allEntries = true)
+    @Override
+    public void updateUser(User user) {
+        userMapper.updateUser(user);
+    }
+
+    // @CachePut 更新缓存策略 根据实际情况清理
+    // @CacheEvict 清理所有
+    @CacheEvict(value = {"users"}, allEntries = true)
+    @Override
+    public void deleteUser(String id) {
+        userMapper.deleteUser(id);
+    }
+}
